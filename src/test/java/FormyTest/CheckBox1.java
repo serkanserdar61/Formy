3ddeea3f6ad12147e1b8d4a93d2cee3ed8e56c36
serkan serdar <serkanserdar61@gmail.com>
2019-11-06 19:01:30 -0500

    inheritance

---
+++ src/test/java/FormyTest/CheckBox1.java
@@ -0,0 +1,56 @@
+package FormyTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class CheckBox1 {
+
+    WebDriver driver;
+    public void GoToTheFormyWebPage(){
+
+        WebDriverManager.chromedriver().setup();
+
+        driver = new ChromeDriver();
+
+        driver.get("http://formy-project.herokuapp.com/");
+
+    }
+
+    public void clickbuttontest(String Checkbox) throws InterruptedException {
+          List<WebElement> button=driver.findElements(By.xpath("/html/body/div/div/li[3]/a"));
+
+        for(WebElement each:button){
+         if(each.getText().equalsIgnoreCase(Checkbox)) {
+             each.click();
+             Thread.sleep(3000);
+             break;
+         } else{
+             System.out.println("I did not found a button for click");
+        }
+    }
+    }
+
+    public void eachboxes() throws InterruptedException {
+        List<WebElement>eachone=driver.findElements(By.xpath("/html/body/div/div/div/div"));
+
+        for(int i=0;i<eachone.size();i++){
+Thread.sleep(3000);
+          String result=  eachone.get(i).getText();
+            System.out.println(result);
+        }
+    }
+
+
+    public void clickable(){
+        List<WebElement>clk=driver.findElements(By.xpath("/html/body/div/div/div/div/input"));
+              for(int i=0;i<clk.size();i++){
+                    clk.get(i).click();
+                }
+    }
+}
+
---
+++ src/test/java/FormyTest/FormyCheckBoxtestCase1.java
@@ -0,0 +1,43 @@
+package FormyTest;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebDriverException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
+public class FormyCheckBoxtestCase1 extends CheckBox1 {
+
+
+
+  @Test
+public void clickbuttontestresult () throws InterruptedException {
+      WebDriverManager.chromedriver().setup();
+     driver=new ChromeDriver();
+    driver.get("http://formy-project.herokuapp.com/");
+    Thread.sleep(3000);
+      clickbuttontest("Checkbox");
+      driver.close();
+  }
+    @Test
+    public void eachboxesresult() throws InterruptedException {
+        GoToTheFormyWebPage();
+        Thread.sleep(3000);
+        clickbuttontest("Checkbox");
+        eachboxes();
+driver.close();
+    }
+  @Test
+    public void clikabletest() throws InterruptedException {
+      GoToTheFormyWebPage();
+      clickbuttontest("Checkbox");
+      Thread.sleep(3000);
+      clickable();
+      driver.close();
+  }
+
+}
